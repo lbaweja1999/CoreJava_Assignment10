e4019498c155fb123732f4ca8bbb2cc055a359af
lbaweja1999 <lakshay17csu093@ncuindia.edu>
2021-03-19 16:55:18 +0530

    Assignment 10 Completed

---
+++ src/SortByNameComparator.java
@@ -0,0 +1,12 @@
+
+import java.util.Comparator;
+
+public class SortByNameComparator implements Comparator{
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		return ((Contact)o1).getContactName().compareTo(((Contact)o2).getContactName());
+	}
+
+}
